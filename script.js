9e95ae615b188aca38fa396e918b43847841678d
Nickwildez <wildeznick@gmail.com>
2024-07-25 19:29:45 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,210 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const homePembukaan = document.querySelector(".home-pembukaan");
+
+    setTimeout(() => {
+        homePembukaan.classList.add("hidden");
+    }, 6000);
+});
+
+
+function howToPageBegining() {
+    const homePembukaan2 = document.querySelector(".home-pembukaan-2");
+    homePembukaan2.style.display = "grid";
+    homePembukaan2.classList.add("clearPembukaan2");
+
+    const headerTop = document.querySelector(".header-top");
+    headerTop.classList.add("showHeaderTop");
+    headerTop.classList.remove("backHeaderTop");
+
+    const homePageBegining = document.querySelector(".home-page-begining");
+    homePageBegining.classList.add("ShowHome");
+
+    const boxInformation = document.querySelector(".box-information");
+    boxInformation.classList.add("showButtonInformation");
+
+    const tabelKerja = document.querySelector(".tabel-jadwal-kerja");
+    tabelKerja.classList.add("showTableKerja");
+
+    const HomeFooter = document.querySelector(".home-footer");
+    HomeFooter.classList.add("showHomeFooter");
+}
+
+function openHomecuciAc() {
+    const showHomeCuciAc = document.querySelector(".home-cuci-ac");
+    showHomeCuciAc.style.display = "grid";
+    showHomeCuciAc.classList.add("showHomeCuciAc");
+    showHomeCuciAc.classList.remove("backHomeCuciAc");
+    showHomeCuciAc.scrollTop = 0;
+
+    const headerTop = document.querySelector(".header-top");
+    headerTop.classList.add("backHeaderTop");
+    headerTop.classList.remove("showHeaderTop");
+
+    const boxInformation = document.querySelector(".box-information");
+    boxInformation.classList.remove("showButtonInformation");
+
+    const showContainerBoxabout = document.querySelector(".container-Boxabout");
+    showContainerBoxabout.classList.add("showBoxabout");
+}
+
+function clearHomeCuciAc() {
+    const showHomeCuciAc = document.querySelector(".home-cuci-ac");
+    showHomeCuciAc.style.display = "grid";
+    showHomeCuciAc.classList.add("backHomeCuciAc");
+    showHomeCuciAc.classList.remove("showHomeCuciAc");
+
+    showHomeCuciAc.addEventListener("animationend", () => {
+        if (!showHomeCuciAc.classList.contains("showHomeCuciAc")) {
+            showHomeCuciAc.style.display = "none";
+        }
+    }, { once: true });
+
+    const headerTop = document.querySelector(".header-top");
+    headerTop.classList.add("showHeaderTop");
+    headerTop.classList.remove("backHeaderTop");
+
+    const boxInformation = document.querySelector(".box-information");
+    boxInformation.classList.add("showButtonInformation");
+}
+
+function openHomeService() {
+    const showHomeService = document.querySelector(".home-service");
+    showHomeService.style.display = "grid";
+    showHomeService.classList.add("showHomeService");
+    showHomeService.classList.remove("backHomeService");
+    showHomeService.scrollTop = 0;
+
+    const headerTop = document.querySelector(".header-top");
+    headerTop.classList.add("backHeaderTop");
+    headerTop.classList.remove("showHeaderTop");
+
+    const boxInformation = document.querySelector(".box-information");
+    boxInformation.classList.remove("showButtonInformation");
+
+    const showContainerService = document.querySelector(".container-home-about-service");
+    showContainerService.classList.add("showAboutService");
+}
+
+function clearHomeService() {
+    const showHomeService = document.querySelector(".home-service");
+    showHomeService.classList.add("backHomeService");
+    showHomeService.classList.remove("showHomeService");
+
+    showHomeService.addEventListener("animationend", () => {
+        if (!showHomeService.classList.contains("showHomeService")) {
+            showHomeService.style.display = "none";
+        }
+    }, { once: true });
+
+    const headerTop = document.querySelector(".header-top");
+    headerTop.classList.add("showHeaderTop");
+    headerTop.classList.remove("backHeaderTop");
+
+    const boxInformation = document.querySelector(".box-information");
+    boxInformation.classList.add("showButtonInformation");
+}
+
+function openHomePesanJasa() {
+    const homePesanJasa = document.querySelector(".home-pesan-jasa");
+    homePesanJasa.style.display = "grid";
+    homePesanJasa.classList.add("showHomePesanJasa");
+    homePesanJasa.classList.remove("backHomePesanJasa");
+    homePesanJasa.scrollTop = 0;
+
+    const headerTop = document.querySelector(".header-top");
+    headerTop.classList.add("backHeaderTop");
+    headerTop.classList.remove("showHeaderTop");
+
+    const formAc = document.querySelector(".form-ac");
+    formAc.classList.add("showFormAc");
+
+    const formService = document.querySelector(".form-service");
+    formService.classList.add("showFormService")
+
+    const boxInformation = document.querySelector(".box-information");
+    boxInformation.classList.remove("showButtonInformation");
+}
+
+function clearHomePesanJasa() {
+    const homePesanJasa = document.querySelector(".home-pesan-jasa");
+    homePesanJasa.style.display = "grid";
+    homePesanJasa.classList.add("backHomePesanJasa");
+    homePesanJasa.classList.remove("showHomePesanJasa");
+
+    homePesanJasa.addEventListener("animationend", () => {
+        if (!homePesanJasa.classList.contains("showHomePesanJasa")) {
+            homePesanJasa.style.display = "none";
+        }
+    }, { once: true })
+
+    const headerTop = document.querySelector(".header-top");
+    headerTop.classList.add("showHeaderTop");
+    headerTop.classList.remove("backHeaderTop");
+
+    const boxInformation = document.querySelector(".box-information");
+    boxInformation.classList.add("showButtonInformation");
+}
+
+document.getElementById('form-ac').addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const jasa = "AC"
+    let sewaJasa = '';
+
+    document.querySelectorAll(".input-radio").forEach((radio) => {
+        if (radio.checked) {
+            sewaJasa = radio.value;
+        }
+    });
+
+    const keluhanLain = document.querySelector("#form-ac .label-1 + input").value;
+    const kuantitas = document.querySelector("#form-ac .label-2 + input").value;
+    const atasNama = document.querySelector("#form-ac .label-3 + input").value;
+    const alamat = document.querySelector("#form-ac .label-4 + input").value;
+
+    const pesan = `jasa: ${jasa}\nsewa jasa: ${sewaJasa}\nkeluhan lain: ${keluhanLain}\nkuantitas: ${kuantitas}\natas nama: ${atasNama}\nalamat: ${alamat}`;
+
+    const whatsappLink = `https://wa.me/+6285774546860?text=${encodeURIComponent(pesan)}`;
+
+    window.open(whatsappLink, '_blank');
+
+    document.getElementById('form-ac').reset();
+});
+
+document.querySelectorAll(".container-input-jasa-ac").forEach((containerInputAc) => {
+    containerInputAc.addEventListener("click", function () {
+        const InputTextNumberAC = containerInputAc.querySelector("input");
+        if (InputTextNumberAC) {
+            InputTextNumberAC.focus();
+        }
+    });
+});
+
+document.querySelectorAll(".container-input-jasa-service").forEach((containerInputService) => {
+    containerInputService.addEventListener("click", function () {
+        const InputTextNumberService = containerInputService.querySelector("input");
+        if (InputTextNumberService) {
+            InputTextNumberService.focus();
+        }
+    });
+});
+
+document.getElementById('form-service').addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const jasaService = "Service"
+
+    const namaBaranngService = document.querySelector("#form-service .label-1 + input").value;
+    const keluhanLainService = document.querySelector("#form-service .label-2 + input").value;
+    const kuantitasService = document.querySelector("#form-service .label-3 + input").value;
+    const atasNamaService = document.querySelector("#form-service .label-4 + input").value;
+    const alamatService = document.querySelector("#form-service .label-5 + input").value;
+
+    const pesan = `jasa: ${jasaService}\nnama Barang: ${namaBaranngService}\nkeluhan lain: ${keluhanLainService}\nkuantitas: ${kuantitasService}\natas nama: ${atasNamaService}\nalamat: ${alamatService}`;
+
+    const whatsappLink = `https://wa.me/+6285774546860?text=${encodeURIComponent(pesan)}`;
+
+    window.open(whatsappLink, '_blank');
+
+    document.getElementById('form-service').reset();
+})
